556faa6aaf270236c7d9cb0778d7023d42719869
daniwinsss <dankdani06@gmail.com>
2026-01-16 15:26:29 +0530

    commit

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ declare module 'express-session' {
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://thumblify-phi.vercel.app'],
     credentials: true
 }));
 app.use(session({
